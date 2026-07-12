dd0e3e68d008ad0b7ab279a90ee2cd031094cc33
Rajesh Maity <rajeshloq2005@gmail.com>
2026-07-13 01:48:29 +0530

    By creating status code

---
+++ Day-5/src/app.js
@@ -0,0 +1,54 @@
+// To create server
+// To config server
+
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+const notes = []
+
+// POST API
+app.post('/notes', (req, res) => {
+    notes.push(req.body)
+
+    console.log(notes);
+
+    res.status(201).json({
+        "message": "Notes Created Successfuly."
+    })
+})
+
+// GET API
+app.get("/notes", (req, res) => {
+    res.status(200).json({
+        notes: notes
+    })
+})
+
+// DELETE API
+app.delete('/notes/:index', (req, res) => {
+    delete notes[req.params.index]
+
+    res.status(204).json({
+        "message":"Notes deleted successfully."
+    })
+})
+
+// PATCH API
+app.patch('/notes/:index',(req,res)=>{
+    notes[req.params.index].description = req.body.description
+    res.status(200).json({
+        "message":"Note Updated Successfully."
+    })
+})
+
+
+
+
+
+
+
+
+
+module.exports = app
