e411ce7ac1460213a0adaee41f953ad56af7db2f
Rajesh Maity <rajeshloq2005@gmail.com>
2026-07-26 20:20:41 +0530

    Add SVG assets and index HTML for frontend setup with backend
    - Created favicon.svg for the application icon.
    - Added icons.svg containing various SVG symbols for UI components.
    - Introduced index.html as the main entry point for the frontend, linking the favicon and necessary assets.

--- Day-9/Backend/src/app.js
+++ Day-9/Backend/src/app.js
@@ -2,11 +2,16 @@
 
 const express = require('express')
 const noteModel = require('./models/notes.model')
-const app = express()
 const cors = require('cors')
+const path = require('path')
 
-app.use(express.json())
+const app = express()
 app.use(cors())
+app.use(express.json())
+// http://localhost:3000/assets/index-C7gR20Rp.js
+// http://localhost:3000/assets/index-C2lCt3AI.css
+// http://localhost:3000/assets/index-C2lCt3AI-2.css ---- <NOT EXIST>
+app.use(express.static('./public'))
 
 app.post('/api/notes', async (req, res) => {
     const { title, description } = req.body
@@ -42,16 +47,19 @@ app.delete('/api/notes/:id', async (req, res) => {
 app.patch('/api/notes/:id', async (req, res) => {
     const id = req.params.id
     const { description } = req.body
-     await noteModel.findByIdAndUpdate(id, { description })
+    await noteModel.findByIdAndUpdate(id, { description })
 
     res.status(200).json({
-        message:"Note updated successfully."
+        message: "Note updated successfully."
     })
 })
 
+console.log(__dirname);
 
 
-
+app.use('*name', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/public/index.html'))
+})
 
 
 
--- Day-9/Frontend/src/App.jsx
+++ Day-9/Frontend/src/App.jsx
@@ -1,41 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
-
 
 function App() {
 
-  const [notes, setNotes] = useState([
-    {
-      title: 'test title',
-      description: 'test description'
-    },
-    {
-      title: 'test title',
-      description: 'test description'
-    },
-    {
-      title: 'test title',
-      description: 'test description'
-    },
-    {
-      title: 'test title',
-      description: 'test description'
-    }
-  ])
+  const [notes, setNotes] = useState([])
 
-  axios.get('http://localhost:3000/api/notes')
-  .then((res)=>{
-    setNotes(res.data.note);
-  })
+  function fetchNotes() {
+    axios.get('http://localhost:3000/api/notes')
+      .then((res) => {
+        setNotes(res.data.note);
+      })
+  }
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  function submitHandler(e) {
+    e.preventDefault()
+
+    const { title, description } = e.target.elements
+
+    axios.post('http://localhost:3000/api/notes', {
+      title: title.value,
+      description: description.value
+    })
+      .then(res => {
+        fetchNotes()
+      })
+  }
+
+  function noteDeleteHandler(noteId) {
+    axios.delete('http://localhost:3000/api/notes/' + noteId)
+      .then(res => {
+        fetchNotes()
+      })
+  }
 
   return (
     <>
+
+      <form className='note-create-form' onSubmit={submitHandler}>
+        <input name='title' type="text" placeholder='Enter title' />
+        <input name='description' type="text" placeholder='Enter description' />
+        <button>Submit</button>
+      </form>
+
       <div className="notes">
         {
           notes.map(note => {
             return <div className="note">
               <h1>{note.title}</h1>
               <p>{note.description}</p>
+
+              <button
+                onClick={() => { noteDeleteHandler(note._id) }}
+              >Delete</button>
             </div>
           })
         }
