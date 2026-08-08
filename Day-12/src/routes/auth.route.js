2375c00cb72cfe2347d8b18badaaa01263d916f3
Rajesh Maity <rajeshloq2005@gmail.com>
2026-08-08 20:01:42 +0530

    feat: initialize Day-13 project with Express, MongoDB, and JWT authentication
    - Added package.json with necessary dependencies
    - Created server.js to set up the Express server
    - Implemented app.js to configure middleware and routes
    - Established database connection in database.js
    - Defined user model in user.model.js
    - Created authentication routes in auth.route.js with user registration functionality

---
+++ Day-12/src/routes/auth.route.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+
+const authRouter = express.Router()
+
+// Have to use /api/auth/register
+authRouter.post('/register', async (req, res) => {
+    const { name, email, password } = req.body
+
+    const isUserAlreadyExist = await userModel.findOne({ email })
+
+    if(isUserAlreadyExist){
+        return res.status(400).json({
+            message:"User Already Exist."
+        })
+    }
+
+    const user = await userModel.create({
+        name, email, password
+    })
+
+    const token = jwt.sign(
+        {
+            id:user._id
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie('jwt_token',token)
+
+    res.status(201).json({
+        message: "User registered successfully.",
+        user,
+        token
+    })
+})
+
+
+
+module.exports = authRouter
