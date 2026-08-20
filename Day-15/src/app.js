3d5aea15d1a8dd869aaea5270a71ab5a4bcf2658
Rajesh Maity <rajeshloq2005@gmail.com>
2026-08-20 21:11:32 +0530

    implement post creation functionality with image upload

--- Day-15/src/app.js
+++ Day-15/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cookiesParser = require('cookie-parser')
 const authRouter = require('./routes/auth.route')
+const postRouter = require('./routes/post.route')
 
 const app = express()
 
@@ -11,5 +12,6 @@ app.use(cookiesParser())
 
 
 app.use('/api/auth', authRouter)
+app.use('/api/posts', postRouter)
 
 module.exports = app
