b5719fb847611bea9fac937f4ee1e89b4847407c
Rajesh Maity <rajeshloq2005@gmail.com>
2026-08-23 19:24:00 +0530

    feat: implement token validation and post creation logic

--- Day-15/src/controllers/post.controller.js
+++ Day-15/src/controllers/post.controller.js
@@ -11,12 +11,42 @@ const imagekit = new ImageKit({
 async function createPostController(req, res) {
     console.log(req.body, req.file);
 
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: 'Token not provided, Unauthorized access'
+        })
+    }
+
+    let decoded = null
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            message: 'user not authorized'
+        })
+    }
+
+    console.log(decoded);
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
-        fileName: 'Test'
+        fileName: 'Test',
+        folder: 'insta-clone'
     })
 
-    res.send(file)
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    })
+
+    res.status(201).json({
+        message: 'Post Created Successfully',
+        post
+    })
+
 }
 
 module.exports = {
