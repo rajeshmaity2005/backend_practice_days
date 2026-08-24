921e73e62901957c2f53cabd9785c78cd3407fc7
Rajesh Maity <rajeshloq2005@gmail.com>
2026-08-24 21:21:38 +0530

    feat: add post retrieval and detail fetching functionality with token validation

--- Day-15/src/controllers/post.controller.js
+++ Day-15/src/controllers/post.controller.js
@@ -9,7 +9,7 @@ const imagekit = new ImageKit({
 })
 
 async function createPostController(req, res) {
-    console.log(req.body, req.file);
+    // console.log(req.body, req.file);
 
     const token = req.cookies.token
 
@@ -28,7 +28,7 @@ async function createPostController(req, res) {
         })
     }
 
-    console.log(decoded);
+    // console.log(decoded);
 
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
@@ -49,6 +49,84 @@ async function createPostController(req, res) {
 
 }
 
+async function getPostController(req, res) {
+    const token = req.cookies.token
+    if (!token) {
+        return res.status(401).json({
+            message: 'Unauthorized Access'
+        })
+    }
+
+    let decoded = null
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            message: 'Token Invalid'
+        })
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        message: 'Post Fetched Successfully',
+        posts
+    })
+
+
+}
+
+async function getPostDetailsController(req, res) {
+    const token = req.cookies.token
+    if (!token) {
+        return res.status(401).json({
+            message: 'Unauthorized Access'
+        })
+    }
+
+    let decoded
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        res.status(401).json({
+            message: 'Invalid Token'
+        })
+    }
+
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if(!post){
+        return res.status(404).json({
+            message:'Post not found.'
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message: 'Forbidden Content.'
+        })
+    }
+
+    return res.status(200).json({
+        message:'Post Fetched Successfully',
+        post
+    })
+
+
+}
+
+
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }
--- Day-15/src/routes/post.route.js
+++ Day-15/src/routes/post.route.js
@@ -9,8 +9,19 @@ const upload = multer({ storage: multer.memoryStorage() })
  * POST /api/posts [protected]
  * - req.body = { caption,image-file }
  */
+postRouter.post('/', upload.single('image'), postController.createPostController)
 
-postRouter.post('/',upload.single('image'), postController.createPostController)
+/**
+ * GET /api/posts/ [protected]
+ */
+postRouter.get('/', postController.getPostController)
+
+/**
+ * GET /api/posts/details/:postId
+ * -return a detail about specific post with the id, also check whether the post belong to the user that is request come from
+ */
+postRouter.get('/details/:postId', postController.getPostDetailsController)
+
 
 
 module.exports = postRouter 
